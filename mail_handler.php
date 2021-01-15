f54a9be35c934b2711cb8227bf95bda4136dba43
lambakobmail <75854401+lambakobmail@users.noreply.github.com>
2021-01-16 02:35:23 +0800

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -12,7 +12,7 @@ if(isset($_POST['submit'])){
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-        echo "Mail Sent. Thank you " . $your_name . ", we will contact you shortly.";
+      header('Location: step2.php');
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
